45f680c53fa6b720cb2d28a73d34c2c8e56749e2
paul <mainaccount@Jupitors-iMac.local>
2017-12-15 18:52:24 +0000

    files updated

---
+++ com/java/MyCalcObject.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class MyCalcObject {
+	
+	public int basicAdd(int x, int y){
+		System.out.println("Add");
+		return x + y;
+	}
+	
+	public int basicDivide(int x, int y){
+		System.out.println("Divide");
+		return x / y;
+	}
+
+}
---
+++ com/test/FirstJUnitTest.java
@@ -0,0 +1,37 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.java.MyCalcObject;
+
+public class FirstJUnitTest {
+	int x = 2;
+	int y = 5;
+
+	@Test
+	public void test() {
+		assertTrue(x == (1 + 1));
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue(y == (4 + 1));
+	}
+	
+	@Test
+	public void add(){
+		MyCalcObject myCalcObj = new MyCalcObject();
+		int result = myCalcObj.basicAdd(2, 2);
+		assertEquals(4, result);
+	}
+	
+	@Test 
+	public void divide(){
+		MyCalcObject myCalcObj = new MyCalcObject();
+		int result = myCalcObj.basicDivide(12, 4);
+		assertEquals(3, result);
+	}
+
+}
