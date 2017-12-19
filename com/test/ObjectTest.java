4c9452b8c64696fe3c7e1d0ed2d8a210340e73f4
paul <mainaccount@Jupitors-iMac.local>
2017-12-19 17:42:53 +0000

    arrayEquals test implemented

--- com/test/ObjectTest.java
+++ com/test/ObjectTest.java
@@ -22,4 +22,31 @@ public class ObjectTest {
 	public void testNotSame() {
 		assertNotSame("Not the same!", s1, s2);
 	}
+	
+	@Test
+	public void arrayTest(){
+		int[] nums1 = {1, 2, 3};
+		int[] nums2 = nums1;
+		
+		assertEquals("Two array pointers", nums1, nums2);
+	}
+	
+	@Test
+	public void arrayTestEquals(){
+		int[] nums1 = {1, 2, 3};
+		int[] nums2 = {1, 2, 3};
+		
+		assertEquals("Two array equals", nums1, nums2);
+	}
+	
+	@Test
+	public void arrayTestEquals2(){
+		int[] nums1 = {1, 2, 3};
+		int[] nums2 = {1, 2, 3};
+		
+		assertArrayEquals("Two array equals", nums1, nums2);
+	}
+	
+	
+	
 }
