1b602f3dfac54a7264b04585d17c47feba14772e
paul <mainaccount@Jupitors-iMac.local>
2017-12-27 17:37:33 +0000

    @Ignore TDD implemented

---
+++ com/java/math/MathCalculations.java
@@ -0,0 +1,20 @@
+package com.java.math;
+
+public class MathCalculations {
+	
+	public int add(int x, int y){
+		return x + y;
+	} 
+	
+	public int subtract(int x, int y){
+		return x - y;
+	} 
+	
+	public int multiply(int x, int y){
+		return x * y;
+	} 
+	
+	public int divide(int x, int y){
+		return 0;
+	} 
+}
---
+++ com/test/MathTest.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.java.math.MathCalculations;
+
+public class MathTest {
+
+	@Test
+	public void addTest() {
+		MathCalculations mathCalc = new MathCalculations();
+		assertEquals("Something went wrong!", 4, mathCalc.add(2, 2));
+	}
+	
+	@Test
+	@Ignore
+	public void subtractTest() {
+		
+	}
+
+}
